ed437c05e3de210c9b4e178625fbb52b2d62b60f
SeuNome <streamflix.org@gmail.com>
2026-07-21 01:44:57 -0300

    teste

--- src/routes/approutes.jsx
+++ src/routes/approutes.jsx
@@ -1,41 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from "../pages/Home/Home";
-import Movies from "../pages/Movies/Movies";
-import Series from "../pages/Series/Series";
-import Search from "../pages/Search/Search";
-import Login from "../pages/Login/Login";
-import Admin from "../pages/Admin/Admin";
-import NotFound from "../pages/NotFound/NotFound";
-
-import PrivateRoute from "./PrivateRoute";
+function Home() {
+  return <h1>Streamflix</h1>;
+}
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-
         <Route path="/" element={<Home />} />
-
-        <Route path="/movies" element={<Movies />} />
-
-        <Route path="/series" element={<Series />} />
-
-        <Route path="/search" element={<Search />} />
-
-        <Route path="/login" element={<Login />} />
-
-        <Route
-          path="/admin"
-          element={
-            <PrivateRoute>
-              <Admin />
-            </PrivateRoute>
-          }
-        />
-
-        <Route path="*" element={<NotFound />} />
-
       </Routes>
     </BrowserRouter>
   );
