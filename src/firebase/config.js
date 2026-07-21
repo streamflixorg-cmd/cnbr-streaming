ef7cf37bee7d2046e3968270fe5a2e208a3b211c
SeuNome <streamflix.org@gmail.com>
2026-07-21 00:49:49 -0300

    ok

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: "AIzaSyDekGvhLbr6e-1buMZb66Vt4uJ2xBXEfCA",
+  authDomain: "streamflix-d860b.firebaseapp.com",
+  projectId: "streamflix-d860b",
+  storageBucket: "streamflix-d860b.firebasestorage.app",
+  messagingSenderId: "667496389728",
+  appId: "1:667496389728:web:272b97bb9bef55d38c7705",
 };
 
 export const app = initializeApp(firebaseConfig);
