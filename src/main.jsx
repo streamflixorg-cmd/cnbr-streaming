82c7c5c13d64c0407d451ec0c0626e0547294a6a
SeuNome <streamflix.org@gmail.com>
2026-07-21 01:41:42 -0300

    io

--- src/main.jsx
+++ src/main.jsx
@@ -4,19 +4,3 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 import "./styles/global.css";
-
-import { StreamflixProvider } from "./context/StreamflixContext";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-
-    <React.StrictMode>
-
-        <StreamflixProvider>
-
-            <App/>
-
-        </StreamflixProvider>
-
-    </React.StrictMode>
-
-);
